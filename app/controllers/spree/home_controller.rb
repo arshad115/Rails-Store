16c71506c092891832a81fd91f26d8386786acb1
Md. Dinar <dinar.cse@gmail.com>
2016-09-22 17:11:15 +0600

    version 2

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -6,8 +6,25 @@ module Spree
     def index
       @searcher = build_searcher(params.merge(include_images: true))
       @products = Spree::Product.available.order('available_on DESC').limit(4);
+
       @searcher1 = build_searcher(taxon: Spree::Taxon.find_by(name: 'Featured'), include_images: true)
-      @products1 = @searcher1.retrieve_products
+      @products1 = @searcher1.retrieve_products.limit(4)
+
+      @searcherp = build_searcher(taxon: Spree::Taxon.find_by(name: 'Popular'), include_images: true)
+      @productsp = @searcherp.retrieve_products.limit(4)
+
+      @searcherb = build_searcher(taxon: Spree::Taxon.find_by(name: 'Bags'), include_images: true)
+      @productsb = @searcherb.retrieve_products.limit(4)
+
+      @searcherm = build_searcher(taxon: Spree::Taxon.find_by(name: 'Mugs'), include_images: true)
+      @productsm = @searcherm.retrieve_products.limit(4)
+
+      @searcherc = build_searcher(taxon: Spree::Taxon.find_by(name: 'Clothing'), include_images: true)
+      @productsc = @searcherc.retrieve_products.limit(4)
+
+      @searchert = build_searcher(taxon: Spree::Taxon.find_by(name: 'T-Shirts'), include_images: true)
+      @productst = @searchert.retrieve_products.limit(4)
+
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
   end
