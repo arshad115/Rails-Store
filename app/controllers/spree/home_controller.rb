21ff39e14127dccddbed2f0b72c1bdb907db94dc
Md. Dinar <dinar.cse@gmail.com>
2016-09-23 20:22:38 +0600

    heroku fix

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -5,7 +5,7 @@ module Spree
 
     def index
       @searcher = build_searcher(params.merge(include_images: true))
-      @products = Spree::Product.available.order('available_on DESC').limit(4);
+      @products = Spree::Product.available.order('available_on DESC').limit(4)
 
       @searcher1 = build_searcher(taxon: Spree::Taxon.find_by(name: 'Featured'), include_images: true)
       @products1 = @searcher1.retrieve_products.limit(4)
