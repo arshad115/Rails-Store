e47bdfce2d9d35179e8ed332e626a7ad9eed51ed
Md. Dinar <dinar.cse@gmail.com>
2016-09-23 21:03:16 +0600

    heroku fix

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '48x48>', # thumbs under image
-    :small => '250x300>', # images on category view
+    :small => '240x300>', # images on category view
     :product => '240x240>', # full product image
     :large => '600x600>' # light box image
   }
